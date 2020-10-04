c1aab0243c27ade4563683864e7187e8f0df4649
Danilo da Silva Ataide <62820994+danilosataide@users.noreply.github.com>
2020-10-04 16:07:53 -0300

    criação da SplashActivity

---
+++ app/src/main/java/br/com/danilosataide/biscoitodasorte/SplashActivity.kt
@@ -0,0 +1,23 @@
+package br.com.danilosataide.biscoitodasorte
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    // fun onCreate é um dos métodos presentes no ciclo de vida da Activity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Abrindo a MainActivity após 5 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Iniciar a tela de Login
+            val mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 5000)
+    }
+}
